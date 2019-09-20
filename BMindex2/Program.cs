a8e670f60fd96c3bb826f9ba0b085b3a0be4d9eb
kebo <bogdan.parubok@hotmail.com>
2019-09-20 23:17:17 +0300

    rounded with Math.Round, code optimization

--- BMindex2/Program.cs
+++ BMindex2/Program.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 
-namespace BMIcalculator
+namespace BMIcalculator2
 {
     class Program
     {
         static void Main()
         {
             float height;       //height is represented in meters with decimal point
-            byte weight;        //weight is usually way below 255 kg hence byte is used because of conserving memory
+            int weight;         //weight is usually way below 255 kg hence byte is used because of conserving memory
             string fullName;
             byte age;           //human does not live over 255 years hence byte is used because of conserving memory 
-            double y;           //BMI according to "new" BMI calculating formula
-            float z;            //BMI according to "old" BMI calculating formula
-            double x;           //exponenting number for new BMI formula
+            double BMInew;      //BMI according to "new" BMI calculating formula
 
             //introduction
             Console.WriteLine("Hello! This is Body Mass Index (BMI) calculator. Insert your height and your weight");
@@ -47,61 +45,59 @@ namespace BMIcalculator
             if (age >= 20 && age <= 65)
             {
                 Console.WriteLine("Thank you. Now, please insert your height in meters.");
-                height = float.Parse(Console.ReadLine());   //Converting inserted height string in meters into decimal floating point number
+                height = float.Parse(Console.ReadLine());       //Converting inserted height string in meters into decimal floating point number
                 Console.WriteLine();
                 Console.WriteLine("Thank you. Now, please insert your weight in kilograms.");
-                weight = byte.Parse(Console.ReadLine());    //converting inserted weight string in kilograms into 8 bit number
-                z = weight / (height * height);             //calculating double precicion point BMI which is assigned name Z
+                weight = int.Parse(Console.ReadLine());         //converting inserted weight string in kilograms into 16 bit number
                 Console.WriteLine();
-                Console.Write("Your BMI is: " + string.Format("{0:0.00}", z));       //string.format converts the number to two decimal points
+                Console.WriteLine($"Your BMI is: {Math.Round(weight / (height * height), 2)}");         //Calculates and rounds float point number to two decimal point number             
                 Console.WriteLine();
                 Console.WriteLine("However, this is according to old calculation method.");
-                Console.WriteLine("According to new calculation method, your BMI is calculated with formula below.");
+                Console.WriteLine("By new calculation method, your BMI is calculated with formula below.");
                 Console.WriteLine("BMI = 1.3 * (weight / (height) by the power of 2.5)");
-                x = Math.Pow(height, 2.5);                  //calculating exponented number of height
-                y = 1.3 * weight / x;                       //calculating BMI according to new formula
+                BMInew = Math.Round(1.3 * weight / (Math.Pow(height, 2.5)), 2);                                        //calculating BMI according to new formula
                 Console.WriteLine();
-                Console.WriteLine("Your BMI according to new calculating formula is: " + string.Format("{0:0.00}", y));      //string.format converts the number to two decimal points
+                Console.WriteLine($"Your BMI according to new calculating formula is: {BMInew}");       //Math.Round rounds float number to two decimal points
                 Console.WriteLine("It may differ from old formula calculation but it also may not.");
 
                 //conditions for advising based on calculated BMI
-                if (y < 16)
+                if (BMInew < 16)
                 {
                     Console.WriteLine();
                     Console.WriteLine("NB! Your BMI is way below acceptable limits. You must see your doctor because your life is in danger!");
                 }
 
-                if (y >= 16 && y < 18.5)
+                if (BMInew >= 16 && BMInew < 18.5)
                 {
                     Console.WriteLine();
                     Console.WriteLine("NB! Your BMI is low, you are underweighted. You must see your doctor because you might have some healt issues.");
                 }
 
-                if (y >= 18.6 && y < 25)
+                if (BMInew >= 18.6 && BMInew < 25)
                 {
                     Console.WriteLine();
                     Console.WriteLine("NB! Your BMI is normal and you are not under nor overweighted.");
                 }
 
-                if (y >= 25.1 && y < 30)
+                if (BMInew >= 25.1 && BMInew < 30)
                 {
                     Console.WriteLine();
                     Console.WriteLine("NB! Your BMI is not good and you are little overweighted.");
                 }
 
-                if (y >= 30.1 && y < 35)
+                if (BMInew >= 30.1 && BMInew < 35)
                 {
                     Console.WriteLine();
                     Console.WriteLine("NB! Your BMI is bad. You are overweighted.");
                 }
 
-                if (y >= 35.1 && y < 40)
+                if (BMInew >= 35.1 && BMInew < 40)
                 {
                     Console.WriteLine();
                     Console.WriteLine("NB! Your BMI is high, you are severely overweighted.");
                 }
 
-                if (y > 40.1)
+                if (BMInew > 40.1)
                 {
                     Console.WriteLine();
                     Console.WriteLine("NB! Your BMI is too high. Your life is in danged because of overweight. Please consult with your doctor on this matter.");
